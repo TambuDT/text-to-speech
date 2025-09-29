312f981ed483c4f81c3b4baa84d23f30791d6629
Davide Tamburrelli <58508206+TambuDT@users.noreply.github.com>
2025-09-29 17:54:19 +0200

    backend in node per funzionalità test to speech e front funzionanti
    creazione del backend in node per funzionalità text to speech,
    integrazione nel frontend fuzionante senza possibilitò di cambiare voce e modificarne i valori

---
+++ backend/js/index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
+
+app.post("/synthesize", async (req, res) => {
+    try {
+        const text = req.body.text;
+        const pitch = req.body.pitch;
+        const voiceName = req.body.voiceName;
+
+        const apiKey = process.env.TEXT2SPEECH_API_KEY;
+
+        const endpoint = `https://texttospeech.googleapis.com/v1/text:synthesize?key=${apiKey}`;
+
+        const payload = {
+            "audioConfig": {
+                "audioEncoding": "LINEAR16",
+                "pitch": pitch,
+                "speakingRate": 1
+            },
+            "input": {
+                "text": text
+            },
+            "voice": {
+                "languageCode": "it-IT",
+               "name": `it-IT-Chirp3-HD-${voiceName}`
+            }
+        };
+
+        const response = await axios.post(endpoint, payload);
+
+        res.json(response.data);
+    } catch (err) {
+        console.error("Errore TTS:", err.response?.data || err.message);
+        res.status(500).json({ error: "Errore durante la sintesi vocale" });
+    }
+});
+
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
--- frontend/t2s/app/components/textarea/textarea.js
+++ frontend/t2s/app/components/textarea/textarea.js
@@ -1,14 +1,43 @@
-import React from 'react'
-import './textarea.css'
+import React, { useState } from 'react';
+import './textarea.css';
+import axios from 'axios';
+
 function Textsection() {
+  const [text, setText] = useState('');
+  const [audioSrc, setAudioSrc] = useState(null);
+  const pitch = 0;
+  const voiceName = "Gacrux";
+
+  async function handleSynthesize() {
+    try {
+      const response = await axios.post('http://localhost:3001/synthesize', { text, pitch, voiceName});
+
+      if (response.data && response.data.audioContent) {
+        const audioBase64 = response.data.audioContent;
+        const audioSrc = `data:audio/mp3;base64,${audioBase64}`;
+        setAudioSrc(audioSrc);
+      } else {
+        console.error("Risposta senza audioContent:", response.data);
+      }
+    } catch (err) {
+      console.error("Errore richiesta:", err);
+    }
+  }
+
   return (
     <div className='textsection-container'>
-        <h1 className='text-section-title'>Text to Speech</h1>
-        <h4 className='text-section-subtitle'>Inserisci il testo da convertire in audio</h4>
-        <textarea className='text-area' placeholder='Inserisci il testo qui...'></textarea>
-        <button className='genera-button'>Genera</button>
+      <h1 className='text-section-title'>Text to Speech</h1>
+      <h4 className='text-section-subtitle'>Inserisci il testo da convertire in audio</h4>
+      <textarea
+        value={text}
+        onChange={e => setText(e.target.value)}
+        className='text-area'
+        placeholder='Inserisci il testo qui...'
+      />
+      <button className='genera-button' onClick={handleSynthesize}>Genera</button>
+      {audioSrc && <audio controls src={audioSrc} />}
     </div>
-  )
+  );
 }
 
-export default Textsection
+export default Textsection;
