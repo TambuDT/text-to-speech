13234537463b9c4aa8f45c96f91d4124244ce2ff
Davide Tamburrelli <58508206+TambuDT@users.noreply.github.com>
2025-09-29 11:13:47 +0200

    aggiunta variabili d'ambiente per protezione chiavi api

--- frontend/t2s/app/appwrite/appwrite.js
+++ frontend/t2s/app/appwrite/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Account } from 'appwrite';
 export const client = new Client();
 
 client
-    .setEndpoint('https://fra.cloud.appwrite.io/v1')
-    .setProject('68d562b8002bba744ba5');
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT);
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
